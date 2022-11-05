ce7ff0bd758171996277e965dc13294ace024930
Rodrigo Bossini <professorbossini@gmail.com>
2022-11-04 21:37:34 -0300

    aplicacao do zero

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react'
-//hooks do React
+import {  StyleSheet,  View } from 'react-native';
 
 export default function App() {
-  const [contador, setContador] = useState(0)
-
-  const incrementar = () => {
-    setContador(contador + 1) 
-  }
+  
   return (
-    <View style={styles.container}>
-      <Text>{contador}</Text>
-      <Button 
-        title='Incrementar'
-        onPress={incrementar}
-      />
+    <View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
