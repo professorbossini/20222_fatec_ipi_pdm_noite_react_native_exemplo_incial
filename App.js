2f9943b5a4aba8fd4d57df5258d64e8e892a02d0
Rodrigo Bossini <professorbossini@gmail.com>
2022-11-04 22:26:48 -0300

    Licao de casa para o Bruno: Implementar o componente PasswordInput

--- App.js
+++ App.js
@@ -1,9 +1,45 @@
-import {  StyleSheet,  View } from 'react-native';
+import {
+  Button,  
+  StyleSheet, 
+  TextInput, 
+  View 
+} from 'react-native';
+
+import { useState } from 'react';
 
 export default function App() {
-  
+  const [lembrete, setLembrete] = useState('')
+  const capturarTexto = (lembreteDigitado) => {
+
+    lembreteDigitado = lembreteDigitado.toUpperCase()
+    // não faça isso!
+    // lembrete = lembreteDigitado
+    // faça isso
+    setLembrete(lembreteDigitado)
+  }
+
+  const adicionarLembrete = () => {
+    console.log("Adicionando...", lembrete)
+    setLembrete('')
+  }  
+  //Single Source of Truth
   return (
-    <View>
+    <View style={{padding: 40}}>
+      <View>
+        <TextInput 
+          style={{borderBottomColor: '#CCC', borderBottomWidth: 2, padding: 12, marginBottom: 4}}
+          placeholder="Lembrar..."
+          onChangeText={capturarTexto}
+          value={lembrete}
+        />
+        <Button 
+          title="OK"
+          onPress={adicionarLembrete}
+        />
+      </View>
+      <View>
+
+      </View>
       
     </View>
   );
